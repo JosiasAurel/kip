564d3a4d22f91ac0c8affb0496fc49d07981f7ae
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-18 16:41:41 +0100

    make pages fetch data

---
+++ client/pages/[note].jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import Head from "next/head";
+
+import styles from "../styles/all.module.css";
+
+const server = "http://localhost:4000";
+
+const NotePage = ({ title, note }) => {
+    console.log({title, note});
+    return (
+        <div>
+            <Head>
+                <link rel="icon" href="data:;base64,="></link>
+            </Head>
+            <main className={styles.notePage}>
+                <article>
+                <h2>{title}</h2>
+                <p>
+                    {note}
+                </p>
+            </article>
+            </main>
+        </div>
+    )
+}
+
+export async function getServerSideProps(ctx) {
+    const noteid = ctx.query.note;
+
+    const noteReq = await fetch(`${server}/getnote`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({noteid: noteid})
+    });
+
+    const noteData = await noteReq.json();
+
+    return {
+        props: noteData
+    }
+}
+
+export default NotePage;
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -5,17 +5,24 @@ import styles from "../styles/all.module.css";
 // import utils
 // import { serialize, deserialize } from "../utils/serializers";
 
+import Head from "next/head";
+
 // server URL
 const server = "http://localhost:4000";
 
 const Home = () => {
     const [title, setTitle] = useState("");
     const [note, setNote] = useState("");
+    const [fetchedNotes, setFetchedNotes] = useState([]);
     
     // text change handler
     function textChangehandler(event, handler) {
         handler(event.target.value);
     }
+
+    useEffect(() => {
+        setFetchedNotes(JSON.parse(localStorage.getItem("notes")) || []);
+    }, []);
 
     // save note handler
     async function saveNote() {
@@ -25,12 +32,35 @@ const Home = () => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({title, note})
-        })
+        }).then(res => res.json()).then(notecid => {
+            if (localStorage.getItem("notes")) {
+                let prevNotes = JSON.parse(localStorage.getItem("notes"));
+
+                prevNotes.push(notecid.id);
+                console.log(notecid);
+
+                alert(`Here is the node id. You can share it ${notecid.id}`);
+
+                setFetchedNotes([...fetchedNotes, notecid.id])
+
+                localStorage.setItem("notes", JSON.stringify(prevNotes));
+
+            } else {
+                localStorage.setItem("notes", JSON.stringify([notecid.id]));
+            }
+
+            // reload page
+            window.location.href = location.href;
+
+        });
     }
     return (
         <div>
-            <header>
-                <h1>KiP - Write and save your notes on the decentralized web</h1>
+            <Head>
+                <link rel="icon" href="data:;base64,="></link>
+            </Head>
+            <header className={styles.header}>
+                <h1>KiP <br /> Write and save your notes on the decentralized web</h1>
             </header>
 
             <div className={styles.controls}>
@@ -45,6 +75,24 @@ const Home = () => {
                     Save
                 </button>
             </span>
+            </div>
+
+            <div className={styles.notes}>
+                {fetchedNotes === [] ?
+                <h2>You have no saved notes, yet!</h2>
+                : 
+                <div className={styles.notes}>
+                    { fetchedNotes.map(note => {
+                    return (
+                        <div className={styles.inote} key={note}>
+                            <p>{note}</p>
+                            <a href={`/${note}`}>Share</a>
+                        </div>
+                    )
+                        }) 
+                    }
+                </div>
+                }
             </div>
         </div>
     )
